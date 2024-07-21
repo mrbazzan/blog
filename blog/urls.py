63dfc70093430f4cc75941d46c4f696126b036af
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-07-21 18:13:58 +0100

    blog/urls: remove misleading comment

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    # /blog/
     path('', views.BlogListView.as_view(), name='home'),
-    # /blog/1/
     path('<int:pk>/', views.BlogDetailView.as_view(), name='detail'),
-    # /blog/new/
     path('new/', views.BlogPostView.as_view(), name='post'),
     path('<int:pk>/edit', views.BlogUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete', views.BlogDeleteView.as_view(), name='delete'),
